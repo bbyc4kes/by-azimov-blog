578fc38301b214d932eb5e78f2a8181797caac52
Maksym Azimov <anthoniotheruiner@gmail.com>
2024-06-19 15:29:50 +0200

    fix: avoid redundancy in composition pattern

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,29 @@
-import Hero from '@/components/Hero'
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
-  return <Hero />
+  return (
+    <section className="max-w-7xl min-h-screen mx-auto flex flex-col lg:flex-row items-center gap-16 lg:gap-20 lg:items-start px-8 py-16 lg:py-36">
+      <div className="flex flex-col h-96 gap-10">
+        <div className="w-full flex flex-col gap-10">
+          <h1 className="text-4xl font-bold">
+            Looking for the fresh content?
+            <br /> This is byAzimov.
+          </h1>
+          <p className="text-lg">
+            Search, write and share ideas using byAzimov!
+            <br /> An open-source blog about web development, programming and
+            more.
+          </p>
+        </div>
+        <Button className="w-48">Read the Blog</Button>
+      </div>
+      <div className="flex justify-center items-center">
+        <img
+          src="/images/byAzimov.png"
+          alt="byAzimov"
+          className="w-64 h-64 rounded-full"
+        />
+      </div>
+    </section>
+  )
 }
